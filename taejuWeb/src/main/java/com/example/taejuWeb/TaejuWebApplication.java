9d5c4b9a99844e0b6faadc376c561f0e0aed310e
taeju <taeju0112@gmail.com>
2024-03-22 12:34:46 +0900

    first commit

---
+++ taejuWeb/src/main/java/com/example/taejuWeb/TaejuWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.taejuWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaejuWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaejuWebApplication.class, args);
+	}
+
+}
